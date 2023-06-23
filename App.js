b2711589d7b996812c9d3a47e15f23556e4c7bf3
Abraham-maker <imnoteverywhere12@gmail.com>
2023-06-23 14:50:26 -0400

    added splashScreen

--- App.js
+++ App.js
@@ -42,20 +42,10 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container} onLayout={onLayoutRootView}>
+    <View onLayout={onLayoutRootView}>
       <Text style={{ fontFamily: "Poppins" }}>
         Open up App.js to start working on your app!
       </Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
